ed6e36838cf42c71ae143670734d0c4c34f007c5
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-02-24 11:14:43 +0300

    publish: Change name in POM

--- itemsadapter-viewbinding/build.gradle.kts
+++ itemsadapter-viewbinding/build.gradle.kts
@@ -1,9 +1,15 @@
+import com.redmadrobot.build.PublishPlugin.Companion.PUBLICATION_NAME
+
 plugins {
     id("redmadrobot.android-library")
     id("redmadrobot.publish")
 }
 
 description = "Use ViewBinding for bindings in ItemsAdapter"
+
+publishing.publications.getByName<MavenPublication>(PUBLICATION_NAME) {
+    pom.name.set("ItemsAdapter ViewBinding")
+}
 
 dependencies {
     api(project(":itemsadapter"))
